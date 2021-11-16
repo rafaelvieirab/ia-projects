09f25fa03d23affeac3dac27ec2ba864c6f76968
Rafael Vieira Barbosa <rafaelviebar@gmail.com>
2021-11-15 21:01:11 -0300

    refactor: ajustes finais

--- Exercicio1_Generic_Algorithm/main.py
+++ Exercicio1_Generic_Algorithm/main.py
@@ -1,15 +1,23 @@
 import random
+from genetic_operators import crossover, mutation
 from selection_functions import selection_by_tournament
 from utils import binary_to_decimal, decimal_to_binary
 
 
-def generate_initial_population(elementInitial, elementFinal):
-    population = [i for i in range(elementInitial, elementFinal+1)]
+def generate_initial_population(limits):
+    (initial, final) = limits
+    population = [i for i in range(initial, final+1)]
     return population
 
+def get_better_generation(generation: list, better_function):
+    while len(generation) > 1:
+        (_, worse) = better_function(generation, 0, 1)
+        generation.pop(worse)
+    better = generation[0]
+    return better
 
-def main(elementInitial, elementFinal, individuals_number, fitness_function, generation_limits, better_function):
-    population = generate_initial_population(elementInitial, elementFinal)
+def main(limits, individuals_number, fitness_function, generation_limits, better_function):
+    population = generate_initial_population(limits)
 
     generation = random.choices(population, k=individuals_number)
     print('\nGeração 0:')
@@ -18,68 +26,29 @@ def main(elementInitial, elementFinal, individuals_number, fitness_function, gen
     for i in range(generation_limits):
         chromosomes = list(map(decimal_to_binary, generation))
         evaluated_chromosomes = list(map(fitness_function, generation))
+        
         selecteds_chromosomes = selection_by_tournament(chromosomes, evaluated_chromosomes, better_function)
+        new_generation = [chromosomes[selected] for selected in selecteds_chromosomes]
+        
         crossed_chromosomes = crossover(chromosomes, selecteds_chromosomes)
-        print("Crossed chromosomes: ", crossed_chromosomes)
+        new_generation += crossed_chromosomes
 
-        mutant_chromosomes = mutation(crossed_chromosomes)
+        mutant_chromosomes = mutation(new_generation)
         generation = list(map(binary_to_decimal, mutant_chromosomes))
 
-        print(f'\nGeração {i}:')
+        print(f'\nGeração {i + 1}:')
         print(generation)
-    return generation
-
-
-def mix_chromosomes(byte1:str, byte2: str):
-    cross_point = random.randint(1, len(byte1) - 2)
-    mixed1 = byte1[:cross_point] + byte2[cross_point:]
-    mixed2 = byte2[:cross_point] + byte1[cross_point:]
-    return (mixed1, mixed2)
-
-
-def crossover(chromosomes: list, selecteds: list, tax = 0.6) -> list:
-    crossed_chromosomes = []
-
-    while(len(selecteds) > 1):
-        c1 = random.choice(selecteds)
-        selecteds.remove(c1)
-        c2 = random.choice(selecteds)
-        selecteds.remove(c2)
-
-        # Todo tem que ver como mudar a quantidade de elementosP
-        (mixed1, mixed2) = mix_chromosomes(chromosomes[c1], chromosomes[c2])
-
-        crossed_chromosomes.append(mixed1)
-        crossed_chromosomes.append(mixed2)
-
-    if len(selecteds) == 1:
-        crossed_chromosomes.append(chromosomes[selecteds[0]])
-
-    return crossed_chromosomes
-
-
-def mutation(chromosomes: list, tax = 0.01) -> list:
-    def mutation2(chromosome: str):
-        pos = random.randint(1, len(chromosome) - 1)
-        new_bit = ''
-        if chromosome[pos] == '0':
-            new_bit = '1'
-        else:
-            new_bit = '0'
-        chromosome = chromosome[:pos-1] + new_bit + chromosome[pos:]
-        return chromosome
-
-    mutant_chromosomes = list(chromosomes)
-    mutant_chromosomes = list(map(mutation2, mutant_chromosomes))
-    return mutant_chromosomes
+    
+    better = get_better_generation(generation, better_function)
+    print(f'\nMelhor valor de x encontrado: {better}, com f({better}) = {fitness_function(better)}')
+    return better
 
 
 if __name__ == "__main__":
-    elementInitial = -10
-    elementFinal = 10
+    limits = (-10, 10)
     individuals_number = 4
     fitness_function = lambda x : x^2 - 3*x + 4
     better_function = lambda array,x,y : (x,y) if array[x] < array[y] else (y, x)
     generation_limits = 5
-    main(elementInitial, elementFinal, individuals_number, fitness_function, generation_limits, better_function)
+    main(limits, individuals_number, fitness_function, generation_limits, better_function)
 
--- Exercicio1_Generic_Algorithm/utils.py
+++ Exercicio1_Generic_Algorithm/utils.py
@@ -7,12 +7,10 @@ def decimal_to_binary(decimal: int, bits_number = 4) -> str:
     
     [_ , binary_unsignal] = bin(decimal).split('b')
 
-    
     while(len(binary_unsignal) < bits_number):
         binary_unsignal = '0' + binary_unsignal
 
     binary += binary_unsignal
-
     return binary
 
 def binary_to_decimal(binary: str) -> int:
